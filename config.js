063c708aff28ba0edfcbea3bc31ba1d20de5c34d
Shiv Majji <smajji@cisco.com>
2019-06-13 14:12:15 -0700

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Shiv',
   githubUrl: 'https://github.com/smajji/shivmajji',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://shivmajji.netlify.com/',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: '',
